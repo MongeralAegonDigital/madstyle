058ea9eac14f7aa77ed90986f6a6f65b27cf6913
Rodrigo Pereira <rodrigo_pereira@outlook.com>
2016-06-01 17:34:29 -0300

    adicionando submodulo no toggle

--- source/javascript/modules/_toggle-field.js
+++ source/javascript/modules/_toggle-field.js
@@ -1,17 +1,36 @@
 var ToggleField = MAD.ToggleField;
 
-ToggleField = function(element, container){
+ToggleField = function(element, container, submodules){
     this.element = element;
     this.container = container;
+    this.submodules = submodules;
 }
 
 ToggleField.prototype.init = function() {
-    this.actionButton = this.element.find('[data-parent-name]');
-
-    this.actionButton.parent().on("change", this.showHide.bind(this));
+    this.submodules.forEach(this.applyFields.bind(this));
 }
 
-ToggleField.prototype.showHide = function(event) {
+ToggleField.prototype.applyFields = function(index) {
+    this.actionButton = this.element.find('[data-parent-name]');
+
+    if (index in this) {
+        this.actionButton.parent().on("change", this[index].bind(this));
+    }
+}
+
+ToggleField.prototype.select = function(event) {
+    var actionValue = this.actionButton.data(),
+        target = this.element.find('[data-name="'+actionValue.parentName+'"]'),
+        selectVerify = $(event.target).find('option:selected').data().parentName;
+
+    if (selectVerify) {
+        target.slideDown();
+    } else {
+        target.slideUp();
+    }
+}
+
+ToggleField.prototype.radio = function(event) {
     var actionValue = this.actionButton.data(),
         target = this.element.find('[data-name="'+actionValue.parentName+'"]');
 
